69a944a34f53132733740f46c430f3742781e045
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-30 15:17:04 +0200

    make build

---
+++ index.f1f1157c.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery__list").addEventListener("click",function(e){e.preventDefault();var t=document.querySelector("#largeImg"),r=e.target.closest(".list-item__link").href;t.src=r});
+//# sourceMappingURL=index.f1f1157c.js.map
